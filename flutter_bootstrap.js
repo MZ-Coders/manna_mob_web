cbdd13b0bf1475d47ce69b8d4c26a20e351351a8
Euler-JS <joaoalbertojose96@gmail.com>
2025-06-20 15:52:29 +0200

    Nova versao do APp com traducao e muito mais...

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2993060926"
+    serviceWorkerVersion: "170184616"
   }
 });
