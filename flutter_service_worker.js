22f8a589ddac923e2476f99494642efc6ecc00c6
Euler-JS <joaoalbertojose96@gmail.com>
2025-09-08 06:56:59 +0200

    App version e URL fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5bb554d97a1e1a9d177f08bccebb2f26",
+const RESOURCES = {"flutter_bootstrap.js": "82f34e624122b38b86340bac5ca159b6",
 "version.json": "72ad31436127869b4e30d934215f76ef",
 "index.html": "9f10543833a538fbebb5297f33498a09",
 "/": "9f10543833a538fbebb5297f33498a09",
-"main.dart.js": "f8904f51f40c2ded4d511313d1fc6abc",
+"main.dart.js": "97b378ff9dd5e82ffabb7edc87bb21d4",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
